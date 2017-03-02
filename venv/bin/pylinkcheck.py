09b429192cd147eabe36ecd7a3a636092ba7fb40
aipacino <540717461@qq.com>
2017-03-02 14:25:51 +0800

    更新了venv环境

--- venv/bin/pylinkcheck.py
+++
@@ -1,4 +0,0 @@
-#!/home/zircon/Desktop/Project/crob/venv/bin/python
-# EASY-INSTALL-SCRIPT: 'pylinkchecker==0.2','pylinkcheck.py'
-__requires__ = 'pylinkchecker==0.2'
-__import__('pkg_resources').run_script('pylinkchecker==0.2', 'pylinkcheck.py')
